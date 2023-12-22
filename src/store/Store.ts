5c60196d33fb82025ad0c5376675b30ccde559f9
archit203 <archit241001@gmail.com>
2023-12-22 15:55:02 +0530

    init store

---
+++ src/store/Store.ts
@@ -0,0 +1,32 @@
+export type UserId = string;
+
+export interface Chat {
+    id: string;
+    userId: UserId;
+    name: string;
+    message: string;
+    upvotes: UserId[];
+}
+
+
+
+export abstract class Store{
+    constructor(){
+
+    }
+    initRoom(roomId: string){
+
+    }
+
+    getChats(roomId: string, limit: number, offset: number){
+
+    }
+
+    addChat(userId: UserId, name: string, message: string , room: string){
+
+    }
+
+    upVote(userId: UserId,room: string, chatId: string){
+
+    }
+}
